58721d4c6123ddbf89efa2ad22f72af716e4137e
Jason Riddle <jallriddle@gmail.com>
2017-04-05 16:18:18 -0400

    Add a simple test

---
+++ test/index.test.js
@@ -0,0 +1,37 @@
+import {expect} from 'chai';
+import {handler as analyzeHandler} from './../src/index';
+import LambdaTester from 'lambda-tester';
+
+// describe("Color Code Converter", function() {
+//   describe("RGB to Hex conversion", function() {
+//     it("converts the basic colors", function() {
+//       var redHex   = converter.rgbToHex(255, 0, 0);
+//       var greenHex = converter.rgbToHex(0, 255, 0);
+//       var blueHex  = converter.rgbToHex(0, 0, 255);
+
+//       expect(redHex).to.equal("ff0000");
+//       expect(greenHex).to.equal("00ff00");
+//       expect(blueHex).to.equal("0000ff");
+//     });
+//   });
+
+//   describe("Hex to RGB conversion", function() {
+//     it("converts the basic colors", function() {
+//       var red   = converter.hexToRgb("ff0000");
+//       var green = converter.hexToRgb("00ff00");
+//       var blue  = converter.hexToRgb("0000ff");
+
+//       expect(red).to.deep.equal([255, 0, 0]);
+//       expect(green).to.deep.equal([0, 255, 0]);
+//       expect(blue).to.deep.equal([0, 0, 255]);
+//     });
+//   });
+// });
+
+describe('handler', function() {
+  it('test failure', function() {
+    return LambdaTester(analyzeHandler)
+      .event({ name: 'Unknown' })
+      .expectError();
+  });
+});
